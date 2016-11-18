d5e447abe49a11cab2243d12aa0551b467084c62
bkwong1990 <bkwong1990@gmail.com>
2016-11-18 14:06:02 -0800

    Took the easy way out and wrote << operator

--- src/NonexistentCommandException.cpp
+++ src/NonexistentCommandException.cpp
@@ -7,7 +7,13 @@ NonexistentCommandException::NonexistentCommandException(std::string &cmd):
 
 }
 
-std::string NonexistentCommandException::getCommand()
+std::string NonexistentCommandException::getCommand() const
 {
 	return badCommand;
+}
+
+std::ostream& operator <<(std::ostream& out, const NonexistentCommandException& ex)
+{
+	out << "The command " << ex.getCommand() << " does not exist. Please look up the list of valid commands for this program.";
+	return out;
 }
